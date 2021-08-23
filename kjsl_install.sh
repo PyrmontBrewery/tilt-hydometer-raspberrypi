c2a6ad3d1fddc49a0d609338a47770ac386dd9e3
Kevin Staunton-Lambert <kevleyski@gmail.com>
2021-08-23 11:50:43 +1000

    Kevs raspi config (64-bit Ubuntu)

--- kjsl_install.sh
+++ kjsl_install.sh
@@ -1,9 +1,20 @@
 # kevs installer on a raspi
 # for Mountain Culture Dolly brew :-)
 
-if [ ! -f "~/kjsl_tilt" ]; then 
+HERE=$PWD
+cp $HERE/kjsl_flow/flow.json $HOME
+
+if [ ! -f ~/kjsl_tilt ]; then 
+
+echo "Installing Tilt Pi"
+
 sudo apt-get update
-sudo apt-get install python3-distutils
+sudo apt-get -y install python3-distutils
+sudo apt-get -y install bluez bluez-hcidump bluez-firmware wpasupplicant
+
+pip install --upgrade pip
+sudo apt-get -y install libbluetooth-dev
+sudo pip3 install bluepy pybluez
 
 git clone https://github.com/kevleyski/aioblescan.git
 
@@ -16,16 +27,18 @@ bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/mast
 cd ~/.node-red
 sudo -H npm install node-red-dashboard@2.15.5
 
-sudo cp ./kjsl_flow/flow.json $HOME
 
 touch ~/kjsl_tilt
 
-echo "You need to reboot then run ~/ksj;_install.sh again"
+echo "You need to reboot then run kjsl_install.sh again"
 exit
 
 fi
 
 # step 2
+
+sudo systemctl enable nodered.service
+sudo systemctl start nodered.service
 
 curl -X POST http://localhost:1880/flows -H "Content-Type: application/json" -H "Node-RED-Deployment-Type: nodes" --data "@/home/pi/flow.json"
 
